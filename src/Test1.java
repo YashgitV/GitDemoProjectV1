f09050497c91c17c547910a37dbc97df8b0cf7c7
yuva <yuva@Yaswanth>
2022-10-31 21:19:46 +0530

    First Commit....

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String attitude = "Never Give Up";
+		if(attitude== "Never Give Up")
+		{
+			System.out.println("goal reached");
+		}
+		else
+		{
+			System.out.println("stay focus");
+		}
+
+	}
+
+}
